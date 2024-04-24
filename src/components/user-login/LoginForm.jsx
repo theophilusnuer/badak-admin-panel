61822ec9d9b575af84e08b9f19943d56b5cb3eb9
pualine <prisypauline@gmail.com>
2024-04-24 10:10:58 +0000

    updating

--- src/components/user-login/LoginForm.jsx
+++ src/components/user-login/LoginForm.jsx
@@ -11,6 +11,9 @@ import Button from "@mui/material/Button";
 import { Link, useNavigate } from "react-router-dom";
 import ErrorMessage from "./errorMessage";
 
+
+
+
 const LoginForm = () => {
   const baseURL = process.env.REACT_APP_URL;
   const navigate = useNavigate();
@@ -48,7 +51,7 @@ const LoginForm = () => {
       if (response.ok) {
         const user = await response.json();
         const tokenGenerated = user.token;
-        sessionStorage.setItem("codeToken", tokenGenerated)
+        sessionStorage.setItem("codeToken", tokenGenerated);
 
         console.log(user);
       } else {
